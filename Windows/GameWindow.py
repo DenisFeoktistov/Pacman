982d0b0720f50dded88f78dedc4aff467ab998fa
denisfeo <dfeoktistov523@icloud.com>
2021-01-28 21:31:56 +0300

    First maze prototype was added. Also some changes in GameWindow.

--- Windows/GameWindow.py
+++ Windows/GameWindow.py
@@ -1,8 +1,11 @@
 import pygame
 
 
+from GameFiles.Game import Game
+
+
 class GameWindow:
-    size = width, height = 1000, 600
+    SIZE = WIDTH, HEIGHT = 1000, 600
 
     def __init__(self, main_interface_class):
         self.main_interface_class = main_interface_class
@@ -11,22 +14,25 @@ class GameWindow:
         self.responder = responder
 
     def show(self):
-        screen = pygame.display.set_mode(GameWindow.size)
+        screen = pygame.display.set_mode(GameWindow.SIZE)
+        game = Game(screen)
 
         self.set_up_screen(screen)
 
-        self.start_main_cycle(screen)
+        self.start_main_cycle(screen, game)
 
-    def start_main_cycle(self, screen):
+    def start_main_cycle(self, screen, game):
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+            screen.fill((0, 0, 0))
+            game.draw()
             pygame.display.flip()
 
     def set_up_screen(self, screen):
         screen.fill((0, 0, 0))
 
     def close(self):
-        pass
+        pygame.display.quit()
