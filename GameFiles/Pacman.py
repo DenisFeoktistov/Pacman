26ad49cf627d17053e9b0c4ad2424cffc6d44c89
dqwed <m.dan3k@yandex.ru>
2021-01-31 15:56:24 +0300

    A few changes in Pacman.

--- GameFiles/Pacman.py
+++ GameFiles/Pacman.py
@@ -26,27 +26,38 @@ class Pacman(pygame.sprite.Sprite):
         self.speed = Pacman.SPEED
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_d]:
+            self.animation()
             self.rotate_image(0)
             self.rect.x += self.speed
         elif keystate[pygame.K_w]:
+            self.animation()
             self.rotate_image(90)
             self.rect.y -= self.speed
         elif keystate[pygame.K_a]:
+            self.animation()
             self.rotate_image(180)
             self.rect.x -= self.speed
         elif keystate[pygame.K_s]:
+            self.animation()
             self.rotate_image(270)
             self.rect.y += self.speed
+        else:
+            self.image = pygame.image.load(Pacman.FRAMES[0])
+            self.transform_image()
         if pygame.sprite.spritecollideany(self, self.borders):
             self.rect.x = self.last_x
             self.rect.y = self.last_y
         self.last_x = self.rect.x
         self.last_y = self.rect.y
 
-
-    def rotate_image(self, angle):
+    def animation(self):
         self.image = pygame.image.load(Pacman.FRAMES[1:][self.counter])
+        self.transform_image()
+        self.counter = (self.counter + 1) % len(Pacman.FRAMES[1:])
+
+    def transform_image(self):
         self.image.set_colorkey(self.image.get_at((0, 0)))
         self.image = pygame.transform.scale(self.image, (50, 50))
+
+    def rotate_image(self, angle):
         self.image = pygame.transform.rotate(self.image, angle)
-        self.counter = (self.counter + 1) % len(Pacman.FRAMES[1:])
