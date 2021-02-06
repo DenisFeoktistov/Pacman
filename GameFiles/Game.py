47d1a6d16ff82f0fbb5bb2c42d52c953fb31bb6e
dqwed <m.dan3k@yandex.ru>
2021-02-06 21:53:33 +0300

    Star class added (not completed, stars are not collecting)

--- GameFiles/Game.py
+++ GameFiles/Game.py
@@ -2,6 +2,7 @@ import pygame
 
 from GameFiles.Maze import Maze
 from GameFiles.Pacman import MazePacman
+from GameFiles.Star import Star
 from GameFiles.Ghost import Ghost
 import GameFiles.SpritesClasses as SpritesClasses
 
@@ -16,6 +17,13 @@ class Game:
         self.pacman = MazePacman(i=0, j=0, sprite_size_x=50, sprite_size_y=50, cycle_time=12 * 20, maze=self.maze)
         self.pacman_sprite = pygame.sprite.GroupSingle(self.pacman)
 
+        self.star_sprites = pygame.sprite.Group()
+        for i in range(self.maze.height):
+            for j in range(self.maze.width):
+                star = Star(i, j, width=8, maze=self.maze)
+                self.star_sprites.add(star)
+
+
         '''self.ghost = Ghost(x=145, y=220, frames=(('data/sprites/ghosts/ghost_up1.png', ), ),
                            sprite_size=50, cycle_iterations=12, cycle_time=360, step=60)
         self.ghost_sprites = pygame.sprite.Group()
@@ -23,11 +31,13 @@ class Game:
 
     def draw(self, screen):
         self.maze.draw()
+        self.star_sprites.draw(screen)
         self.pacman_sprite.draw(screen)
         # self.ghost_sprites.draw(screen)
 
     def update(self):
         self.pacman.update()
+        self.star_sprites.update()
         # self.ghost_sprites.update()
 
     def handle(self, event):
