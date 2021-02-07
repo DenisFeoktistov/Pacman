c5c9cfd244beb85edf42f5de5d7c2902e98aa6f2
dqwed <m.dan3k@yandex.ru>
2021-02-07 18:28:13 +0300

    Code cleaning.

--- GameFiles/Game.py
+++ GameFiles/Game.py
@@ -5,8 +5,6 @@ from GameFiles.Maze import Maze
 from GameFiles.Pacman import MazePacman
 from GameFiles.Star import Star
 from GameFiles.Ghost import MazeGhost
-import GameFiles.SpritesClasses as SpritesClasses
-from GameFiles.Ghost import MazeGhost
 from Responders.GameResponder import GameResponder
 
 
@@ -14,22 +12,28 @@ class Game:
     def __init__(self, screen):
         self.score = 0
         self.responder = GameResponder(self)
-        # We use 12 *, just because it is better for game. If you choose something like 77 or 113, game will be started,
-        # but you can find some problems, because of float numbers, that will be in GeneralSprite.
-        self.maze = Maze(x=20, y=100, height=8, width=16, cell_width=12 * 5, cell_height=12 * 5, screen=screen,
-                         density=20, game=self)
-        self.maze.generate()
-
-        self.pacman = MazePacman(i=0, j=0, sprite_size_x=50, sprite_size_y=50, cycle_time=12 * 20, maze=self.maze)
-        self.pacman_sprite = pygame.sprite.GroupSingle(self.pacman)
-
+        self.create_maze(screen)
+        self.create_pacman()
         self.create_ghosts()
+        self.create_stars()
+
+    def create_stars(self):
         self.star_sprites = pygame.sprite.Group()
         for i in range(self.maze.height):
             for j in range(self.maze.width):
                 star = Star(i, j, width=8, maze=self.maze)
                 self.star_sprites.add(star)
 
+    def create_pacman(self):
+        self.pacman = MazePacman(i=0, j=0, sprite_size_x=50, sprite_size_y=50, cycle_time=12 * 20, maze=self.maze)
+        self.pacman_sprite = pygame.sprite.GroupSingle(self.pacman)
+
+    def create_maze(self, screen):
+        # We use 12 *, just because it is better for game. If you choose something like 77 or 113, game will be started,
+        # but you can find some problems, because of float numbers, that will be in GeneralSprite.
+        self.maze = Maze(x=20, y=100, height=8, width=16, cell_width=12 * 5, cell_height=12 * 5, screen=screen,
+                         density=20, game=self)
+        self.maze.generate()
 
     def draw(self, screen):
         self.maze.draw()
