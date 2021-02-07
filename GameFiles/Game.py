04594c3f0748cca60cc86ebeffdf34c0acfd97f0
denisfeo <dfeoktistov523@icloud.com>
2021-02-07 14:52:40 +0300

    Just bag-fixes after merge.

--- GameFiles/Game.py
+++ GameFiles/Game.py
@@ -4,7 +4,7 @@ from random import randint
 from GameFiles.Maze import Maze
 from GameFiles.Pacman import MazePacman
 from GameFiles.Star import Star
-from GameFiles.Ghost import Ghost
+from GameFiles.Ghost import MazeGhost
 import GameFiles.SpritesClasses as SpritesClasses
 from GameFiles.Ghost import MazeGhost
 from Responders.GameResponder import GameResponder
@@ -17,7 +17,7 @@ class Game:
         # We use 12 *, just because it is better for game. If you choose something like 77 or 113, game will be started,
         # but you can find some problems, because of float numbers, that will be in GeneralSprite.
         self.maze = Maze(x=20, y=100, height=8, width=16, cell_width=12 * 5, cell_height=12 * 5, screen=screen,
-                         density=40, game=self)
+                         density=20, game=self)
         self.maze.generate()
 
         self.pacman = MazePacman(i=0, j=0, sprite_size_x=50, sprite_size_y=50, cycle_time=12 * 20, maze=self.maze)
@@ -30,11 +30,6 @@ class Game:
                 star = Star(i, j, width=8, maze=self.maze)
                 self.star_sprites.add(star)
 
-
-        '''self.ghost = Ghost(x=145, y=220, frames=(('data/sprites/ghosts/ghost_up1.png', ), ),
-                           sprite_size=50, cycle_iterations=12, cycle_time=360, step=60)
-        self.ghost_sprites = pygame.sprite.Group()
-        self.ghost_sprites.add(self.ghost)'''
 
     def draw(self, screen):
         self.maze.draw()
@@ -49,7 +44,7 @@ class Game:
         self.pacman.update()
         self.ghost_sprites.update()
         self.star_sprites.update()
-        # self.ghost_sprites.update()
+        self.ghost_sprites.update()
 
     def handle(self, event):
         self.pacman.handle(event)
