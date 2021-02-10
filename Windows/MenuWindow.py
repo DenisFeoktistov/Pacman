f980ab06af4b19e16fa558bd5dc40d4565475172
dqwed <m.dan3k@yandex.ru>
2021-02-10 22:39:36 +0300

    Main menu prototype completed.

--- Windows/MenuWindow.py
+++ Windows/MenuWindow.py
@@ -36,6 +36,7 @@ class MenuWindow:
         self.set_play_button()
         self.set_rules_button()
         self.set_leaders_button()
+        self.set_back_button()
 
     def buttons_click_check(self, mouse_pos):
         if self.play_button.collidepoint(mouse_pos):
@@ -47,6 +48,8 @@ class MenuWindow:
         if self.leaders_button.collidepoint(mouse_pos):
             self.running = False
             self.main_window.switch_to_score()
+        if self.back_button.collidepoint(mouse_pos):
+            self.running = False
 
     def set_up_screen(self):
         self.screen.fill((0, 0, 0))
@@ -67,21 +70,28 @@ class MenuWindow:
         self.screen.blit(self.text_rules, self.rules_button)
         self.screen.blit(self.text_play, self.play_button)
         self.screen.blit(self.text_leaders, self.leaders_button)
+        self.screen.blit(self.text_back, self.back_button)
 
     def set_play_button(self):
         self.text_play = self.font.render('Играть', False, (255, 255, 255))
         self.play_button = self.text_play.get_rect()
-        self.play_button.center = (self.main_window.WIDTH // 2, self.main_window.HEIGHT // 4)
+        self.play_button.center = (self.main_window.WIDTH // 2, self.main_window.HEIGHT // 2 - 100)
 
     def set_rules_button(self):
         self.text_rules = self.font.render('Правила', False, (255, 255, 255))
         self.rules_button = self.text_rules.get_rect()
-        self.rules_button.center = (self.main_window.WIDTH // 2, self.main_window.HEIGHT // 4 + 55)
+        self.rules_button.center = (self.main_window.WIDTH // 2, self.main_window.HEIGHT // 2 - 50)
 
     def set_leaders_button(self):
         self.text_leaders = self.font.render('Рекорды', False, (255, 255, 255))
         self.leaders_button = self.text_leaders.get_rect()
-        self.leaders_button.center = (self.main_window.WIDTH // 2, self.main_window.HEIGHT // 4 + 110)
+        self.leaders_button.center = (self.main_window.WIDTH // 2, self.main_window.HEIGHT // 2)
+
+    def set_back_button(self):
+        font = pygame.font.Font('data/fonts/pixel1.ttf', 25)
+        self.text_back = font.render('Выход', False, (255, 255, 255))
+        self.back_button = self.text_back.get_rect()
+        self.back_button.center = (self.main_window.WIDTH // 2, self.main_window.HEIGHT - 50)
 
     def set_font(self):
         self.font = pygame.font.Font('data/fonts/pixel1.ttf', 40)
