0ae6957828c153f2e2ea54a41d97dcb1d00e1c62
dqwed <m.dan3k@yandex.ru>
2021-02-12 00:16:44 +0300

    Just volume was set.

--- SubsidiaryFiles/Sounds.py
+++ SubsidiaryFiles/Sounds.py
@@ -3,7 +3,9 @@ import pygame.mixer as mixer
 
 mixer.init()
 chomp_sound = mixer.Sound('data/sounds/pacman_chomp.wav')
+chomp_sound.set_volume(0.35)
 death_sound = mixer.Sound('data/sounds/pacman_death.wav')
+death_sound.set_volume(0.35)
 beginning_sound = mixer.Sound('data/sounds/pacman_beginning.wav')
 eat_bonus_sound = mixer.Sound('data/sounds/pacman_eatfruit.wav')
 eat_ghost_sound = mixer.Sound('data/sounds/pacman_eatghost.wav')
