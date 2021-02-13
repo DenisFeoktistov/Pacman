8bff89f571fb24ae2a6db5bbfee4aa49ac2453fb
denisfeo <dfeoktistov523@icloud.com>
2021-02-13 18:42:52 +0300

    Little fix in main.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from PyQt5.QtWidgets import QApplication
 import pygame
-import sys
 
 
 from Windows.MainWindow import MainWindow
@@ -17,10 +15,8 @@ if __name__ == '__main__':
     pygame.font.init()
     pygame.mixer.init()
 
-    app = QApplication(sys.argv)
     game = App()
 
     pygame.quit()
     pygame.font.quit()
     pygame.mixer.quit()
-    sys.exit(app.exec())
